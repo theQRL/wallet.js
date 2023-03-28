d204a9ad0be4996c34cb2b164970c7c95cec7f40
JP Lomas <jp@theqrl.org>
2023-03-29 00:27:22 +0100

    Fix byte count check and throw on binToMnemonic()

--- src/utils/mnemonic.js
+++ src/utils/mnemonic.js
@@ -1,32 +1,32 @@
-import { WordList } from "../qrl/wordlist.js";
-
-export function SeedBinToMnemonic(input) {
-    return binToMnemonic(input);
-}
+import { WordList } from '../qrl/wordlist.js';
 
 function binToMnemonic(input) {
-    if (String(input).length % 3 != 0) {
-        console.error("byte count needs to be a multiple of 3");
+  if (input.length % 3 !== 0) {
+    throw new Error('byte count needs to be a multiple of 3');
+  }
+  let mnemonic = '';
+  let separator = '';
+  for (let nibble = 0; nibble < input.length * 2; nibble += 3) {
+    const p = nibble >> 1;
+    const b1 = input[p];
+    let b2 = 0;
+    if (p + 1 < input.length) {
+      b2 = input[p + 1];
     }
-    let mnemonic = ''
-    var separator = "";
-    let buf = Buffer.alloc(input.length * 4);
-    for (let nibble = 0; nibble < input.length * 2; nibble += 3) {
-        let p = nibble >> 1;
-        let b1 = input[p];
-        let b2 = 0;
-        if ((p + 1) < input.length) {
-            b2 = input[p + 1]
-        }
-        let idx = 0;
-        if (nibble % 2 == 0) {
-            idx = (b1 << 4) + (b2 >> 4);
-        } else {
-            idx = ((b1 & 0x0F) << 8) + b2;
-        }
-        mnemonic += separator + WordList[idx];
-        separator = " ";
+    let idx = 0;
+    if (nibble % 2 === 0) {
+      idx = (b1 << 4) + (b2 >> 4);
+    } else {
+      idx = ((b1 & 0x0f) << 8) + b2;
     }
-    // return buf.toString()
-    return mnemonic;
+    mnemonic += separator + WordList[idx];
+    separator = ' ';
+  }
+  return mnemonic;
 }
+
+export function SeedBinToMnemonic(input) {
+  return binToMnemonic(input);
+}
+
+export default SeedBinToMnemonic;
